1d9acb7ac1f74f3eb6ad54593fc1e62736bb0dd8
Arash Karampour <karampour.arash@yahoo.com>
2026-01-05 18:19:23 +0100

    adding lombok to the project and modifying the User class with it

--- src/main/java/com/example/store/entities/User.java
+++ src/main/java/com/example/store/entities/User.java
@@ -1,7 +1,15 @@
 package com.example.store.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users") // this will bind this entity to the table with the name users
 public class User {
@@ -16,35 +24,4 @@ public class User {
     @Column(nullable = false, name = "password")
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
